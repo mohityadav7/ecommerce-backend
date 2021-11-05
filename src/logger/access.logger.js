efa28f657e6a564a124facea36361e56794d26f9
Mohit Yadav <mohityadav1921@gmail.com>
2021-11-06 00:02:24 +0530

    ⚡update logger for prod

--- src/logger/access.logger.js
+++ src/logger/access.logger.js
@@ -5,18 +5,18 @@ const config = require('../config');
 const isProduction = config.env === 'production';
 
 const accessLogger = morgan(
-    isProduction ? 'combined' : 'dev',
-    isProduction
-        ? {
-              stream: rfs.createStream('access.log', {
-                  interval: '1M',
-                  path: 'logs',
-                  size: '1Mb',
-                  maxFiles: 10,
-                  encoding: 'utf8',
-              }),
-          }
-        : {}
+    isProduction ? 'combined' : 'dev'
+    // isProduction
+    //     ? {
+    //           stream: rfs.createStream('access.log', {
+    //               interval: '1M',
+    //               path: 'logs',
+    //               size: '1Mb',
+    //               maxFiles: 10,
+    //               encoding: 'utf8',
+    //           }),
+    //       }
+    //     : {}
 );
 
 module.exports = accessLogger;
