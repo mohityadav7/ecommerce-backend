7b9e2aa266002ed8022d4d821257203e57d55f6b
Mohit Yadav <mohityadav1921@gmail.com>
2021-11-06 00:07:54 +0530

    ⚡update logger for prod (#7)
    log logs to console instead of saving to files, because apparently it is not a good idea to save logs in files on heroku

--- src/logger/prod.logger.js
+++ src/logger/prod.logger.js
@@ -4,25 +4,34 @@ const { timestamp, combine, errors, label, json } = format;
 module.exports = createProductionLogger = (tag) => {
     return createLogger({
         level: 'info',
+        // format: combine(
+        //     timestamp(),
+        //     label({ label: tag }),
+        //     errors({ stack: true }),
+        //     json()
+        // ),
         format: combine(
-            timestamp(),
+            timestamp({
+                format: 'DD-MM-YYYY HH:mm:ss',
+            }),
             label({ label: tag }),
             errors({ stack: true }),
-            json()
+            logFormat
         ),
         transports: [
             //
             // - Write all logs with level `error` and below to `error.log`
             // - Write all logs with level `info` and below to `combined.log`
             //
-            new transports.File({
-                filename: 'logs/error.log',
-                level: 'error',
-            }),
-            new transports.File({
-                filename: 'logs/combined.log',
-                level: 'info',
-            }),
+            // new transports.File({
+            //     filename: 'logs/error.log',
+            //     level: 'error',
+            // }),
+            // new transports.File({
+            //     filename: 'logs/combined.log',
+            //     level: 'info',
+            // }),
+            new transports.Console(),
         ],
     });
 };
